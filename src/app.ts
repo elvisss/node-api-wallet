2b33e725faf657475670cb232d6a1c23be6700fd
Elvis Saavedra <elvis.saavedra.segura@gmail.com>
2022-12-08 23:44:56 -0500

    preparing to prod

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import express, { Application } from 'express'
 import { loadControllers } from 'awilix-express'
 import dotenv from 'dotenv'
 import loadContainer from './container'
+import { config } from '../config'
 
 dotenv.config({
   path: `${__dirname}/../config/${process.env.APP_ENV}.env`,
@@ -15,6 +16,6 @@ const app: Application = express()
 loadContainer(app)
 
 app.use(express.json())
-app.use(loadControllers('controllers/*.ts', { cwd: __dirname }))
+app.use(loadControllers(`controllers/*.${config.dev ? 'ts' : 'js'}`, { cwd: __dirname }))
 
 export { app }
