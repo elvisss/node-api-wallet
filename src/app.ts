0652de73d1f27b0fa8bf3fc23910c0900fa69fde
Elvis Saavedra <elvis.saavedra.segura@gmail.com>
2022-12-11 00:46:26 -0500

    cleaning up

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,7 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
-process.env.APP_ENV = process.env.APP_ENV || 'dev'
-
 import express, { Application } from 'express'
 import { loadControllers } from 'awilix-express'
-import dotenv from 'dotenv'
 import loadContainer from './container'
 import { config } from '../config'
-
-dotenv.config({
-  path: `${__dirname}/../config/${process.env.APP_ENV}.env`,
-})
 
 const app: Application = express()
 
--- src/common/controllers/base.controller.ts
+++ src/common/controllers/base.controller.ts
@@ -3,16 +3,14 @@ import { ApplicationException } from '../exception/application.exception'
 
 export abstract class BaseController {
   handleException(err: any, res: Response) {
-    console.log({ err })
-
     if (!(err instanceof ApplicationException)) {
-      res.status(500)
+      res.status(400)
       res.send(err.message)
       return
-      /* throw new Error(err.message) */
     }
 
     res.status(err.status)
     res.send(err.message)
+    return
   }
 }
